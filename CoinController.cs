d84fb90b8893ecffd24dbe1a9f8139482c667822
Hackbright Student <student@hackbrightacademy.com>
2021-06-16 16:18:36 -0700

    Creates functionality for coin to rotate and bob up and down.

--- CoinController.cs
+++ CoinController.cs
@@ -16,4 +16,22 @@ public class CoinController : MonoBehaviour
         startPos = transform.position;
         targetPos = startPos + new Vector3(0, bobHeight, 0);
     }
+
+    void Update()
+    {
+        // Position for coint to bob towards or away.
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, bobSpeed * Time.deltaTime);
+
+        // Rotates coin.
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+
+        // Changes bob direction so it will bob between different positions.
+        if(transform.position == targetPos)
+        {
+            if(targetPos == startPos)
+                targetPos = startPos + new Vector3(0, bobHeight, 0);
+            else if(targetPos == startPos + new Vector3(0, bobHeight, 0))
+                targetPos = startPos;
+        }
+    }
 }
