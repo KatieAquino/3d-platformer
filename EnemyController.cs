b9bf3371d4067cedf51e122e160358e47797d875
Hackbright Student <student@hackbrightacademy.com>
2021-06-16 15:26:55 -0700

    Begins script for enemy model movement.

---
+++ EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public Vector3 offsetEndPos;
+
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+    void Awake()
+    {
+        startPos = transform.position;
+        targetPos = startPos + offsetEndPos;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+    
+    }
+}
